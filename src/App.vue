d01a3186d8c6d737af648b683a9bc2b5375347b7
Ishwor <ishworkhatri26@yahoo.com>
2019-01-03 19:16:10 -0500

    If else condition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
     <h1 v-bind:class="decoration"> {{msg}} </h1>
+    <hr>
+    <strong>If Else</strong>
+    <p v-if="seen">This paragraph will display if the seen value is true otherwise hide it</p>
+    <p v-else="seen">I am display if seen value is false</p>
+    <hr>
+    <strong>Else If</strong>
+    <p v-if="type == 'A'">I will be display if type is A</p>
+    <p v-else-if="type == 'B'">I will be display if type is B</p>
+    <p v-else-if="type == 'C'">I will be display if type is C</p>
+    <p v-else="type">I will be display if type is non of A, B, C and D</p>
   </div>
 </template>
 
@@ -10,7 +20,9 @@ export default {
   data () {
     return {
       decoration: 'decorationClass',
-      msg: 'Welcome To Vue Basic'
+      msg: 'Welcome To Vue Basic',
+      seen: false,
+      type: 'A'
     }
   }
 }
@@ -19,7 +31,6 @@ export default {
 
 <!-- This style will be global and applied to all over -->
 <style lang="scss">
-  * { margin: 0rem; }
 
   h1 {
     text-align: center;
