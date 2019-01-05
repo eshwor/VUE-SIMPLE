7d7de68c2d2b8f5abf47643f721c680667b272c3
Ishwor <ishworkhatri26@yahoo.com>
2019-01-05 13:30:06 -0500

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-    {{msg}}
-    <app>tet</app>
+    <strong>Event Handling</strong>
+    <button type="button" @click="counter += 1">INCREASE</button>
+    <p> {{counter}} </p>
+    <hr>
+    <strong>Method Event Handling</strong>
+    <button type="button" @click="greeting">EVENT HANDLING</button>
+    <p>{{ greeting() }}</p>
+    <hr>
+    <strong>INPUT BINDING</strong>
+    <p>Input Text Binding</p>
+    <input type="text" name="" value="" placeholder='text goes here' v-model="message">
+    <p>Binding Result : {{message}} </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  el: '#app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      counter: 0,
+      message: "VUE Developer"
+    }
+  },
+  methods: {
+    greeting(event){
+      if(event){
+        alert(event.target.attributes);
+      }
     }
   }
 }
