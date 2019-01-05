4b888f64fa9fe4b93e714694fe68420ae953bb81
Ishwor <ishworkhatri26@yahoo.com>
2019-01-05 12:38:25 -0500

    basic Vue JS

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,51 @@
     <p v-else-if="type == 'B'">I will be display if type is B</p>
     <p v-else-if="type == 'C'">I will be display if type is C</p>
     <p v-else="type">I will be display if type is non of A, B, C and D</p>
+    <hr>
+    <hr>
+    <hr>
+    <strong>For Loop</strong>
+    <p>Array loops with index</p>
+    <ul>
+      <li v-for="item, index in items"> {{index}} - {{item.message}} </li>
+    </ul>
+    <hr>
+    <strong>Object value loop and it's key pairs</strong>
+    <ul>
+      <li v-for="(value, key, index) in object"> {{index}} :-  {{key}} : {{value}} </li>
+    </ul>
+    <hr>
+    <p v-for="count in count">I will be keep looping belongs count value</p>
+    <hr>
+    <strong>Array push method</strong>
+    <ul>
+      <li v-for="pushMsg in pushMsg"> {{pushMsg}}</li>
+      <button type="button" @click="pushMsg.push('D')">Push Something</button>
+    </ul>
+    <hr>
+    <strong>Using once even its override</strong>
+    <p v-once>This is never chance and use it once forever : {{neverchange}}</p>
+    <hr>
+    <strong>Raw HTML</strong>
+    <h2 v-html="htmlWay"></h2>
+    <hr>
+    <strong>Attributes Change</strong>
+    <button type="button" name="bindName" v-bind:disabled="isButtonDisabled">ATTRIBUTES BUTTON</button>
+    <hr>
+    <strong>Modifiers</strong>
+    <p>Modifiers goes here</p>
+    <hr>
+    <strong>Passing Arguments</strong>
+    <button type="button" @click="incrementFunction(5)">Increment the Counter</button>
+    <p> {{counterNumber}} </p>
+    <hr>
+    <strong>Modifiers</strong>
+    <input type="text" @keyup.stop.enter="keyModifier()">
+    <hr><hr><hr>
+    <strong>Methods vs computed vs watcher [Need To Learn More Details]</strong>
+    <p> Original Message: {{welcomeMsg}} </p>
+    <p> New Message: {{ printFunction() }} </p>
+    <p> New Message: {{ computedPrintFunction }} </p>
   </div>
 </template>
 
@@ -22,7 +67,41 @@ export default {
       decoration: 'decorationClass',
       msg: 'Welcome To Vue Basic',
       seen: false,
-      type: 'A'
+      type: 'A',
+      items: [
+        {message: "Message One"},
+        {message: "Message Two"}
+      ],
+      object: {
+        name: "Vue Lover",
+        age: 1,
+        hubby: "Vue Developer"
+      },
+      count: 2,
+      pushMsg: ['A','B','C'],
+      neverchange: "Neverever change",
+      htmlWay: '<p>This is the paragraph</p>',
+      isButtonDisabled: true,
+      bindName: 'buttonName',
+      counterNumber: 0,
+      welcomeMsg: "Old Welcome Message"
+    }//return
+  },//data end
+  methods: {
+    incrementFunction(pass){
+      this.counterNumber += pass;
+    },
+    keyModifier(){
+      alert("Alert !");
+    },
+    printFunction(){
+      return this.welcomeMsg = "Methods New Welcome Message";
+    }
+  },
+  computed: {
+    computedPrintFunction(){
+      //return this.welcomeMsg.split('').reverse().join('');
+      return this.welcomeMsg = "Computed New Message";
     }
   }
 }
@@ -34,6 +113,17 @@ export default {
 
   h1 {
     text-align: center;
+  }
+
+  button {
+    padding: 1rem 2rem;
+    display: block;
+    background-color: green;
+    color: white;
+    margin: 1rem 0;
+  }
+  button[disabled]{
+    background-color: #DDD;
   }
 
   .decorationClass {
